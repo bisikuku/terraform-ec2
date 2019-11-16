11c9dd6f77c5723e64bde7bf1f3e68672c4a0ec0
Sameer Meher <sam.tiku@gmail.com>
2019-11-16 23:20:14 +0530

    First terraform check-in

---
+++ instance.tf
@@ -0,0 +1,19 @@
+resource "aws_instance" "example" {
+  ami           = var.AMIS[var.AWS_REGION]
+  instance_type = "t2.micro"
+  provisioner "local-exec" {
+    command = "echo ${aws_instance.example.private_ip} >> private_ips.txt"
+  }
+}
+
+# to allocate a static public ip
+resource "aws_eip" "example_eip" {
+  instance = aws_instance.example.id
+  vpc      = true
+}
+
+
+output "public_ip" {
+  #value = aws_instance.example.public_ip   # when allocated dynamically by aws
+  value = aws_eip.example_eip.public_ip # when generated using eip
+}
